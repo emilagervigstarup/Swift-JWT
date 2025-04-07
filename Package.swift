2a36d3b23f97890a29d60361241b68be542fb88f
Emil Starup <3866442+emilagervigstarup@users.noreply.github.com>
2025-04-07 12:01:16 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -34,8 +34,7 @@ let package = Package(
         .package(name: "CryptorRSA", url: "https://github.com/Kitura/BlueRSA.git", from: "1.0.202"),
         .package(name: "Cryptor", url: "https://github.com/Kitura/BlueCryptor.git", from: "2.0.1"),
         .package(name: "CryptorECC", url: "https://github.com/Kitura/BlueECC.git", from: "1.2.200"),
-        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "2.0.0"),
-        .package(url: "https://github.com/Kitura/KituraContracts.git", from: "2.0.1")
+        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "2.0.0")
     ],
     targets: [
         .target(name: "SwiftJWT", dependencies: [
